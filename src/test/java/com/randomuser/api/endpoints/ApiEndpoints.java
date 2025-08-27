9f026be82546fe3baa61a1ebd46261e25c119089
Andrey <erm.qa@ya.ru>
2025-08-28 01:03:35 +0300

    update edpoints

--- src/test/java/com/randomuser/api/endpoints/ApiEndpoints.java
+++ src/test/java/com/randomuser/api/endpoints/ApiEndpoints.java
@@ -1,4 +1,9 @@
 package com.randomuser.api.endpoints;
 
 public class ApiEndpoints {
+    public static final String USERS = "/";
+    public static final String USERS_WITH_PARAMS = USERS + "?results={results}";
+    public static final String USERS_WITH_GENDER = USERS + "?gender={gender}";
+    public static final String USERS_WITH_NATIONALITY = USERS + "?nat={nat}";
+    public static final String USERS_WITH_SEED = USERS + "?seed={seed}";
 }
